c80b0a768303ca088b27e145b73a22b99cff0f09
olamidecode <olaakinde16@gmail.com>
2019-07-05 10:24:05 +0200

    added comments and a loop.js file

--- js/weather.js
+++ js/weather.js
@@ -2,15 +2,19 @@ function myFunction(){
     var temperature = document.getElementById("temp").value;
 // Creating a temp variable
     
-// Giving the temp is greater than 25 degree
-    if (temperature >=25) {
+//  Giving the temp is greater than 25 degree
+        if (temperature >=25) {
         alert("T-shirts, shorts, shades and some sun block.");
-        }else if (temperature >=12 && temperature <=15) {
+//  Giving the temp is greater than 12 but less than 15
+        } else if (temperature >=12 && temperature <=15) {
             alert("Wear a wind breaker jacket");
+//  Giving the temp is greater than -10 but less than -1
         } else if (temperature >=-10 && temperature <=-1) {
             alert("Dress warmly and bring out your shovel cause its snowy");
+//  Giving that the temp is less than 10 but greater than 1   
         } else if (temperature <=10 && temperature >=1) {
             alert("wear a rain coat and take your umbrela"); 
+//  Giving that if any of the above values aren't entered then its invalid   
         } else {
             alert("Not a valid weather temperature");
         }
