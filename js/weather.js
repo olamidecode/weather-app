6af1c1fb31cdc23c327c9336b5cddeade62a43d2
olamidecode <olaakinde16@gmail.com>
2019-07-05 14:45:20 +0200

    changed my alert message to p

--- js/weather.js
+++ js/weather.js
@@ -4,19 +4,19 @@ function myFunction(){
     
 //  Giving the temp is greater than 25 degree
         if (temperature >=25) {
-        alert("T-shirts, shorts, shades and some sun block.");
+        document.getElementById("text").innerHTML = "T-shirts, shorts, shades and some sun block.";
 //  Giving the temp is greater than 12 but less than 15
         } else if (temperature >=12 && temperature <=15) {
-            alert("Wear a wind breaker jacket");
+        document.getElementById("text").innerHTML ="Wear a wind breaker jacket";
 //  Giving the temp is greater than -10 but less than -1
         } else if (temperature >=-10 && temperature <=-1) {
-            alert("Dress warmly and bring out your shovel cause its snowy");
+        document.getElementById("text").innerHTML ="Dress warmly and bring out your shovel cause its snowy";
 //  Giving that the temp is less than 10 but greater than 1   
         } else if (temperature <=10 && temperature >=1) {
-            alert("wear a rain coat and take your umbrela"); 
+        document.getElementById("text").innerHTML ="wear a rain coat and take your umbrela"; 
 //  Giving that if any of the above values aren't entered then its invalid   
         } else {
-            alert("Not a valid weather temperature");
+        document.getElementById("text").innerHTML ="Not a valid weather temperature";
         }
     
     }
